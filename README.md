fff0ecf37962337c2e3fc67fd14d5379e7b12249
leitosama <fs.leito@gmail.com>
2025-03-29 18:41:29 +0700

    fixes

--- README.md
+++ README.md
@@ -3,3 +3,8 @@ RBF calendar
 
 ## API
 https://org.infobasket.su/
+https://org.infobasket.su/Widget/TeamGames/3204?format=json
+https://org.infobasket.su/Widget/TeamInfo/3204?format=json
+
+## VideoID
+Все трансляции беру из iframe'ов
--- rbf2ics.py
+++ rbf2ics.py
@@ -1,26 +1,46 @@
-from datetime import datetime, timedelta, date
-import json
+from datetime import datetime, timedelta, date, timezone
 import re
-from typing import Tuple, List, Dict
-from datetime import datetime, timedelta, timezone
+from typing import Tuple
+import json
+import logging
 
+import requests
 
 HOME_ARENAID = 11745
-HOME_ADDRESS = 'Север\\nУчительская улица\, 61\, Новосибирск\, Новосибирская область\, Россия\, 630110'
+HOME_ADDRESS = r'Север\\nУчительская улица\, 61\, Новосибирск\, Новосибирская область\, Россия\, 630110'
+HOME_TEAMID = 3204
 HOME_EMOJI = "🏠"
 VIDEO_EMOJI = "🛜"
 
-def get_video(s: str) -> str:
-    pattern = r"src='//([^\\]+)'"
-    return f"https://{re.search(pattern, s).group(1)}"
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
-def get_dt(e: dict) -> Tuple[str, str]:
-    d = e['GameDateTimeMoscow']
+def get_team_info(team_id: int):
+    team_info = json.loads(requests.get(f"https://org.infobasket.su/Widget/TeamInfo/{team_id}?format=json").text, object_hook=lambda d: SimpleNamespace(**d))
+    team_info.json = team_info
+    return team_info
+
+def get_team_games(team_id: int):
+    team_games = json.loads(requests.get(f"https://org.infobasket.su/Widget/TeamGames/{team_id}?format=json").text, object_hook=lambda d: SimpleNamespace(**d))
+    for game in team_games:
+        game.json = game
+    return team_games
+
+def get_video(s: str) -> str:
+    if not s:
+        return "-"
+    regexp = r"src=('|\")(https:|)\/\/([-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*))('|\")"
+    search = re.search(regexp, s)
+    logger.debug(f"VideoID: {s}")
+    return f"https://{search.group(3)}"
+
+def get_datetime(item) -> Tuple[str, str]:
+    d = item.json.GameDateTimeMoscow
     tzid = "Europe/Moscow"
-    moscow_offset = timedelta(hours=3)
-    moscow_tz = timezone(moscow_offset)
+    moscow_tz = timezone(timedelta(hours=3))
     if d is None:
-        d = e['GameLocalDate']
+        d = item.json.GameLocalDate
         d = date.fromtimestamp(int(d[6:-2])/1000)
         dtstart=d.strftime('%Y%m%d')
         dtend=(d+timedelta(days=1)).strftime('%Y%m%d')
@@ -37,55 +57,51 @@ def get_dt(e: dict) -> Tuple[str, str]:
     event_dtend = f"TZID={tzid}:{dtend}"
     return event_dtstart, event_dtend
 
-def get_events(j: str) -> List[Dict]:
-    d = json.loads(j)
-    events = []
-    for e in d:
-        print(e['GameID'])
-        video = get_video(e['VideoID'])
-        watch_emoji = None  
-        event_location = None
-        if e['ArenaId'] == HOME_ARENAID:
-            watch_emoji = HOME_EMOJI
-            event_location = HOME_ADDRESS
-        else:
-            watch_emoji = VIDEO_EMOJI
-            event_location = video
+def make_ics_headers(team_name: str, team_id: int):
+    return f"BEGIN:VCALENDAR\nVERSION:2.0\nPRODID:RBF2ICS\nNAME:БК {team_name}\nX-WR-CALNAME:БК {team_name}\nDESCRIPTION:Календарь матчей РФБ ❤️ команды. Адрес для домашней площадки работает только для БК Новосибирск\nX-WR-CALDESC:RBF2ICS\nSOURCE;VALUE=URI:https://n8n.leito.tech/webhook/rbf2ics?teamId={team_id}\nREFRESH-INTERVAL;VALUE=DURATION:PT60M\nX-PUBLISHED-TTL:PT60M\nX-WR-TIMEZONE:UTC\nMETHOD:PUBLISH\nCALSCALE:GREGORIAN\n"
 
-        event_summary = f"🏀 {watch_emoji} {e['ShortTeamNameAru']} vs {e['ShortTeamNameBru']}"
-        event_dtstart, event_dtend = get_dt(e)
-        event_description = f"Трансляция: {video}\nАрена: {e['ArenaRu']}"
-        events.append({
-            'summary': event_summary,
-            'description': event_description,
-            'location': event_location,
-            'dtstart': event_dtstart,
-            'dtend': event_dtend
-        })
-    return events
+def make_ics_event(item, team_id: int) -> str:
+    logging.debug(f"GameID: {item.json.GameID}")
+    link = f"https://competitions.russiabasket.ru/games/{item.json.GameID}"
+    if item.json.VideoID:
+        video = get_video(item.json.VideoID)
+    else:
+        video = "-"
+    logging.debug(f"VideoID: {video}")
+    watch_emoji = None   
+    if item.json.ArenaId == HOME_ARENAID and item.json.TeamAid == team_id:
+        watch_emoji = HOME_EMOJI
+        location = HOME_ADDRESS
+    else:
+        watch_emoji = VIDEO_EMOJI
+        location = video
+    summary = f"🏀 {watch_emoji} {item.json.ShortTeamNameAru} vs {item.json.ShortTeamNameBru}"
+    logging.debug(f"Summary: {summary}")
+    dtstart, dtend = get_datetime(item)
+    logging.debug(f"{dtstart} - {dtend}")
+    description = f"Трансляция: {video}\nАрена: {item.json.ArenaRu}\nСсылка на матч: {link}"
+    # logging.debug(description)
+    ics_content = f"BEGIN:VEVENT\nSUMMARY:{summary}\nDESCRIPTION:{description}\nLOCATION:{location}\nDTSTART;{dtstart}\nDTEND;{dtend}\nEND:VEVENT\n"
 
+    return ics_content
 
-def generate_ics(events, file_name='calendar.ics'):
-    ics_content = "BEGIN:VCALENDAR\nVERSION:2.0\nCALSCALE:GREGORIAN\n"
-
-    for event in events:
-        ics_content += f"""BEGIN:VEVENT
-SUMMARY:{event['summary']}
-DESCRIPTION:{event['description']}
-LOCATION:{event['location']}
-DTSTART;{event['dtstart']}
-DTEND;{event['dtend']}
-END:VEVENT
-"""
+def make_ics_calendar(team_id: int, team_info, team_games) -> str:
+    ics_content = make_ics_headers(team_name=team_info.json.CurrentTeamName.CompTeamShortNameRu, team_id=team_id)
+    for item in team_games:
+        ics_content += make_ics_event(item,team_id)
 
     ics_content += "END:VCALENDAR"
+    return ics_content
 
-    with open(file_name, 'w', encoding='utf-8') as file:
-        file.write(ics_content)
+# for local
+# team_id = 3204
+# team_info = get_team_info(team_id)
+# team_games = get_team_games(team_id)
 
-    print(f"ICS file '{file_name}' created successfully.")
+# for n8n
+# team_id = _('Webhook').item.json.query.teamId
+# team_info = _('TeamInfo').item
+# team_games = _input.all()
 
-
-nsk = open("sample.json","r").read()
-events = get_events(nsk)
-generate_ics(events)
+# ics_calendar = make_ics_calendar(team_id,team_info,team_games)
+# return [{'json':{'text':ics_calendar}}]
