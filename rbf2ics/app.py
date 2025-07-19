fd224bd46c09d8ee09b4c5e3e38a8b431e815747
leitosama <fs.leito@gmail.com>
2025-07-19 22:25:40 +0700

    [rbf2ics] Add feature to select teams

--- rbf2ics/app.py
+++ rbf2ics/app.py
@@ -14,9 +14,20 @@ class RequestFailedException(Exception):
         self.status_code = status_code
         self.response_text = response_text
 
+ARENAS = {
+    11745: {
+        "address": r'Север\nУчительская улица\, 61\, Новосибирск\, Новосибирская область\, Россия\, 630110',
+        "2gis": "https://go.2gis.com/pzJ4S",
+        "ymaps": "https://yandex.ru/maps/-/CHHnUK7O"
+    },
+    11926: {
+        "address": r'ДС Динамо\nУлица Лавочкина\, 32\, Москва\, Россия\, 125581',
+        "2gis": "https://go.2gis.com/FTXFY",
+        "ymaps": "https://yandex.ru/maps/-/CHHnaJYm"
+    }
+}
 
 HOME_ARENAID = 11745
-HOME_ADDRESS = r'Север\\nУчительская улица\, 61\, Новосибирск\, Новосибирская область\, Россия\, 630110'
 HOME_TEAMID = 3204
 HOME_EMOJI = "🏠"
 VIDEO_EMOJI = "🛜"
@@ -24,7 +35,7 @@ RBF_API_URL = "https://org.infobasket.su".rstrip("/")
 
 logging.basicConfig(level=logging.DEBUG)
 
-def get_team_info(team_id: int):
+def get_team_info(team_id: int) -> dict:
     team_info_r = requests.get(f"{RBF_API_URL}/Widget/TeamInfo/{team_id}?format=json")
     if team_info_r.status_code != 200:
         raise RequestFailedException(
@@ -36,7 +47,7 @@ def get_team_info(team_id: int):
     team_info.json = team_info
     return team_info
 
-def get_team_games(team_id: int):
+def get_team_games(team_id: int) -> dict:
     team_games_r = requests.get(f"{RBF_API_URL}/Widget/TeamGames/{team_id}?format=json")
     if team_games_r.status_code != 200:
         raise RequestFailedException(
@@ -83,7 +94,7 @@ def get_datetime(item) -> Tuple[str, str]:
 def make_ics_headers(team_name: str, team_id: int):
     return f"BEGIN:VCALENDAR\nVERSION:2.0\nPRODID:RBF2ICS\nNAME:БК {team_name}\nX-WR-CALNAME:БК {team_name}\nDESCRIPTION:Календарь матчей РФБ ❤️ команды. Адрес для домашней площадки работает только для БК Новосибирск\nX-WR-CALDESC:RBF2ICS\nSOURCE;VALUE=URI:https://n8n.leito.tech/webhook/rbf2ics?teamId={team_id}\nREFRESH-INTERVAL;VALUE=DURATION:PT60M\nX-PUBLISHED-TTL:PT60M\nX-WR-TIMEZONE:UTC\nMETHOD:PUBLISH\nCALSCALE:GREGORIAN\n"
 
-def make_ics_event(item, team_id: int) -> str:
+def make_ics_event(item, team_id: int, arena_id: int) -> str:
     logging.debug(f"GameID: {item.json.GameID}")
     link = f"https://competitions.russiabasket.ru/games/{item.json.GameID}"
     if item.json.VideoID:
@@ -92,9 +103,9 @@ def make_ics_event(item, team_id: int) -> str:
         video = "Ссылка не опубликована :("
     logging.debug(f"VideoID: {video}")
     watch_emoji = None
-    if int(item.json.ArenaId) == HOME_ARENAID and int(item.json.TeamAid) == int(team_id):
+    if int(item.json.ArenaId) == arena_id and int(item.json.TeamAid) == int(team_id):
         watch_emoji = HOME_EMOJI
-        location = HOME_ADDRESS
+        location = ARENAS[arena_id]['address']
     else:
         watch_emoji = VIDEO_EMOJI
         if video == "Ссылка не опубликована :(":
@@ -107,16 +118,16 @@ def make_ics_event(item, team_id: int) -> str:
     logging.debug(f"Summary: {summary}")
     dtstart, dtend = get_datetime(item)
     logging.debug(f"{dtstart} - {dtend}")
-    description = f"Трансляция: {video}\\nАрена: {item.json.ArenaRu}\\nСсылка на матч: {link}"
+    description = f"Трансляция: {video}\\nАрена: {item.json.ArenaRu}\\nСсылка на матч: {link}\\n2ГИС: {ARENAS[arena_id]['2gis']}\\nЯндекс.Карты: {ARENAS[arena_id]['ymaps']}"
     # logging.debug(description)
     ics_content = f"BEGIN:VEVENT\nSUMMARY:{summary}\nDESCRIPTION:{description}\nLOCATION:{location}\nDTSTART;{dtstart}\nDTEND;{dtend}\nEND:VEVENT\n"
 
     return ics_content
 
-def make_ics_calendar(team_id: int, team_info, team_games) -> str:
+def make_ics_calendar(team_id: int, arena_id: int, team_info: dict, team_games: dict) -> str:
     ics_content = make_ics_headers(team_name=team_info.json.CurrentTeamName.CompTeamShortNameRu, team_id=team_id)
     for item in team_games:
-        ics_content += make_ics_event(item,team_id)
+        ics_content += make_ics_event(item,team_id,arena_id)
 
     ics_content += "END:VCALENDAR"
     return ics_content
@@ -132,12 +143,13 @@ def make_ics_calendar(team_id: int, team_info, team_games) -> str:
 
 if __name__ == "__main__":
     # for local
-    team_id = 3204
+    team_id = HOME_TEAMID
+    arena_id = HOME_ARENAID
     team_info = get_team_info(team_id)
     team_games = get_team_games(team_id)
     
-    ics_calendar = make_ics_calendar(team_id,team_info,team_games)
-    
+    ics_calendar = make_ics_calendar(team_id,arena_id, team_info,team_games)
+    print(ics_calendar)
 
 
 def lambda_handler(event, context):
@@ -163,9 +175,10 @@ def lambda_handler(event, context):
     """
     query_params = event.get("queryStringParameters") or {}
     team_id = int(query_params.get("team_id", HOME_TEAMID))
+    arena_id = int(query_params.get("arena_id", HOME_ARENAID))
     team_info = get_team_info(team_id)
     team_games = get_team_games(team_id)
-    ics_content = make_ics_calendar(team_id, team_info, team_games)
+    ics_content = make_ics_calendar(team_id, arena_id, team_info, team_games)
     logging.debug(ics_content)
     
 
